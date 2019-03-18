d1595d259a063e1ed3a61c64fa6bf2614b646929
florent417 <flole@live.dk>
2019-03-18 11:01:19 +0100

    Add project files.

---
+++ DAB_7.1_exercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_7._1_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
